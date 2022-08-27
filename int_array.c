f7794e3ad2b4256880823e92c8cf33566d77f2ed
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2022-08-27 23:55:09 +0100

    Update int_array.c

--- int_array.c
+++ int_array.c
@@ -6,13 +6,13 @@ struct int_array {
 };
 
 int get(const struct int_array* self, int x) {
-    if (x >= (*self).capacity) return -1;
-    return *((*self).data + x);
+    if (x >= self->capacity) return -1;
+    return *(self->data + x);
 }
 
 int set(const struct int_array* self, int index, int value) {
-    if (index >= (*self).capacity) return 0;
-    *((*self).data + index) = value;
+    if (index >= self->capacity) return 0;
+    *(self->data + index) = value;
     return 1;
 }
 
@@ -37,7 +37,7 @@ void print(const struct int_array* self) {
     }
 }
 
-struct int_array New(int cap) {
+struct int_array new(int cap) {
     int* _arr = (int*)calloc(cap, sizeof(int));
     printf("_arr: %p", _arr);
     struct int_array arr = { cap, _arr };
@@ -45,13 +45,13 @@ struct int_array New(int cap) {
 }
 
 int main(void) {
-    struct int_array arr = New(12);
+    struct int_array arr = new(12);
     print(&arr);
     int v = set(&arr, 1, 12);
     print(&arr);
+    printf("Def [1] -> %d\n", get(&arr, 1));
     // free(arr.data);
     dispose(&arr);
     print(&arr);
     //dispose(&arr);
-
 }
